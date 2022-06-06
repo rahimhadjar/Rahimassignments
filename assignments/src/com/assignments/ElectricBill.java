c8fde4d8461cab1b9ad28cdc6fa8206b0a3a57cb
Rahim <Rahim@10.0.0.33>
2022-06-05 22:18:07 -0400

    update

--- assignments/src/com/assignments/ElectricBill.java
+++ assignments/src/com/assignments/ElectricBill.java
@@ -6,10 +6,25 @@ public class ElectricBill {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		double previous;
+		double current;
 		Scanner scan=new Scanner(System.in);   // Scanner class object is created
-		System.out.println("enter the number of units");
-		double units=scan.nextInt();
-		System.out.println("units given is:"+units);
+		System.out.println("========================================");
+		System.out.println("WELCOM TO ELECTIC BILL MANAGMENT PROGRAM");
+		System.out.println("========================================");
+
+		
+		
+		System.out.println("Please enter previous month meter reading: ");
+		previous=scan.nextInt();
+		
+		
+		System.out.println("Please enter current month meter reading: ");
+		current=scan.nextInt(); 
+		
+	    double units=current-previous;
+		
+		System.out.println("Your total consumption for this month is: "+units+ " units");
 		scan.close();
 	    
 		
@@ -31,7 +46,7 @@ public class ElectricBill {
 		} else if(units>400) {
 			billpay=50*1+50*2+100*3+200*4+(units-400)*5;
 		}  	
-		System.out.println("your total bill is:  Rs"+billpay);
+		System.out.println("your total bill is:  $"+billpay);
 	}
 
 }

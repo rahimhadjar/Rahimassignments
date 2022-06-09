600f49240cd01ad9a8aea98011c5d1a1252834b5
Rahim <Rahim@10.0.0.33>
2022-06-08 20:20:36 -0400

    Assignment 4

--- assignments/src/com/assignments/Display2dArray.java
+++ assignments/src/com/assignments/Display2dArray.java
@@ -7,6 +7,7 @@ public class Display2dArray {
 		int arr1[][]= {{1},{2,1,2},{3,2,1,2,3},{4,3,2,1,2,3,4},{5,4,3,2,1,2,3,4,5}};  //2-D
 		
 		
+		
 		for (int outer[] : arr1) {  
 			for (int inner : outer) {  
 				System.out.print(inner+" ");
